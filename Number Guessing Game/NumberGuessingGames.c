7b679113f438e6d9f0126456cf4731147b855341
Swarnali Mitra <swarnalighosh1974@gmail.com>
2025-12-12 08:01:45 +0530

    Add files via upload

---
+++ Number Guessing Game/NumberGuessingGames.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int random,guess,no_of_guess=0;
+    scanf(time(NULL));
+    printf("Welcome to the world of guessing numbers.");
+    random=rand()%100+1; // Generating between 0 to 100
+    do{
+        printf("Please enter your guess(1 to 100) : ");
+        scanf("%d",&guess);
+        no_of_guess++;
+        if(guess<random){
+            printf("Guess larger number.\n");
+        }else if(guess>random){
+            printf("Guess a smaller number.\n");
+        }else {
+            printf("Congratulations !!! You have successfully giessed the number in %d attempts.",no_of_guess);
+        }
+        
+    }while(guess!=random);
+    printf("\n Bye Bye, Thanks for playing.");
+    printf("\n Developed by : Swarnali Ghosh");
+}
